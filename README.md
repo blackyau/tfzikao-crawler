1a1ff164fe21779e2f855c79aa1abefa1da67e68
blackyau <blackyau426@gmail.com>
2018-10-14 16:22:13 +0800

    初始版本

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # tfzikao-crawler
+
 四川自考网爬虫,用于爬取历年试题的链接和试题名称
+
+## 依赖
+
+``` pip install requests```
+
+``` pip install pandas```
+
+``` pip install beautifulsoup4```
+
+## 运行
+
+``` git clone https://github.com/blackyau/tfzikao-crawler.git ```
+
+或 下载源码 https://github.com/blackyau/tfzikao-crawler/archive/master.zip
+
+在项目文件夹内运行``` python get.py ```
+
+## 说明
+
+默认抓取 [理工类专业](http://www.tfzikao.com/xlks/lnst/gxlzy/) 的历年试题,你可以修改 ```get.py``` 第7行的 url 变量为你需要对应专业的试题
+
+默认抓取前 132 页的内容,你可以修改 ```get.py``` 第31行的数字为你 期望的页数+1 
+
+公共课真题 ```http://www.tfzikao.com/xlks/lnst/ggkzt/```
+
+经济类专业 ```http://www.tfzikao.com/xlks/lnst/jjxzy/```
+
+法学类专业 ```http://www.tfzikao.com/xlks/lnst/fxlzy/```
+
+文学类专业 ```http://www.tfzikao.com/xlks/lnst/wxlzy/```
+
+理工类专业 ```http://www.tfzikao.com/xlks/lnst/gxlzy/```
+
+教育类专业 ```http://www.tfzikao.com/xlks/lnst/jyl/```
+
+管理类专业 ```http://www.tfzikao.com/xlks/lnst/gllzy/```
+
+农学类专业 ```http://www.tfzikao.com/xlks/lnst/nxlzy/```
+
+医学类专业 ```http://www.tfzikao.com/xlks/lnst/yxlzy/```
+
+其它类 ```http://www.tfzikao.com/xlks/lnst/qtl/```
+
+# TODO
+
+- 自动获得总页数
+- 交互式选择不同的专业的历年试题
+- 优化代码
---
+++ get.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas
+
+
+# ggkzt 公共课公共课真题|jjxzy 经济类专业|fxlzy 法学类专业|wxlzy 文学类专业|gxlzy 理工类专业|jyl 教育类专业|gllzy 管理类专业|nxlzy 农学类专业|yxlzy 医学类专业|qtl 其它类
+url1 = 'http://www.tfzikao.com/xlks/lnst/gxlzy/'
+num = 2
+href_data = []
+title_data = []
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36',
+    'Referer': 'http://www.tfzikao.com/xlks/lnst/',
+    'host': 'Host: www.tfzikao.com',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6'
+}
+
+# 抓取第一页内容
+r = requests.get(url1,headers=headers)
+r.encoding = 'UTF-8'
+ul_data = BeautifulSoup(r.text,'lxml').find('ul', class_='mod-list mod-list-date mod-list-long')
+for i in ul_data.find_all('a'):
+        href_data.append(i['href'])
+        title_data.append(i['title'])
+pd = pandas.DataFrame({'num':'1','url':href_data,'name':title_data})
+pd.to_csv('tfzikaoDATA.csv',mode='a',header=None)
+title_data.clear()
+href_data.clear()
+
+# 抓取第一页之后的内容,下行数字应为总页数+1
+while num < 133:
+    url = url1 + 'List_' + str(num) + '.html'
+    r = requests.get(url,headers=headers)
+    r.encoding = 'UTF-8'
+    soup = BeautifulSoup(r.text,'lxml')
+    ul_data = soup.find('ul', class_='mod-list mod-list-date mod-list-long')
+    for i in ul_data.find_all('a'):
+        href_data.append(i['href'])
+        title_data.append(i['title'])
+    num = num +1
+    pd = pandas.DataFrame({'num':num,'url':href_data,'name':title_data})
+    pd.to_csv('tfzikaoDATA.csv',mode='a',header=None)
+    title_data.clear()
+    href_data.clear()
